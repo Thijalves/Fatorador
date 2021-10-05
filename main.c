dd5ad782e68a8231ef43d067fc1b75ce69c7ac97
unknown <thijalves@gmail.com>
2021-10-05 19:59:06 -0300

    My second commit

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+    int a; 
+    printf("Esse programa fatora numeros INTEIROS\n");
+    printf("Informe o numero a ser fatorado: ");
+    scanf("%d", &a);
+
+    while (1){
+        
+    }
+    
+}
